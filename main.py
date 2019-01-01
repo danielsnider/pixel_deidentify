9b150db5525cea817bdfed0f0d2464365b15025d
Daniel Snider <danielsnider12@gmail.com>
2018-12-31 16:00:49 -0800

    better

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from itertools import chain
 
+
 def flatten(foo):
     for x in foo:
         if hasattr(x, '__iter__'):
@@ -15,6 +16,8 @@ def flatten(foo):
                 yield y
         else:
             yield x
+
+
 
 # Load Image
 filename = "94862596.dcm"
@@ -25,68 +28,51 @@ im_resized = cv2.resize(img, dsize=(img.shape[1]*4, img.shape[0]*4), interpolati
 
 # Build list of PHI to look for: MNR, FirstName, LastName
 PHI = [dicom.PatientID, dicom.PatientName.split('^')] # PatientName ist ypically: FirstName^LastName
-PHI = [item for sublist in PHI for item in sublist] # Flatten list
-list(flatten(PHI))
+PHI = list(flatten(PHI))
+
+dicom_metadata = {}
+[dicom_metadata.__setitem__(key,dicom.get(key)) for key in dicom.dir() if key not in ['PixelData']]
 
 # Find PHI matches in image
-ocr_match(im_resized,'') 
 
-def ocr_match(image, search_strings, tesseract_config=None):
+row = pd.DataFrame([{ 'FileName' : filename,
+                     'FilePath' : filename,
+                     'Modality' : '??',
+                     'PatientName' : dicom.PatientName,
+                     'PatientID' : dicom.PatientID,
+                     'PHI' : [PHI],
+                     'MetaData' : dicom_metadata,
+                   }])
+
+
+
+def ocr_match(row, image, search_strings, ocr_num=None):
+  # Do OCR
+  tesseract_config = '--oem %d --psm 3' % ocr_num
   detection = pytesseract.image_to_data(image,config=tesseract_config, output_type=pytesseract.Output.DICT)
   detected_strings = [text for text in detection['text'] if text] # remove empty strings
-  for search_string in search_strings:
+  row['ocr%d_detected_strings' % ocr_num] = detected_strings
+
+  # Transform to uppercase because case might not match from image pixels to metadata
+  detected_strings = [x.upper() for x in detected_strings]
+  search_strings = [x.upper() for x in search_strings]
+
+  # Search for each given string
+  for string_num, search_string in enumerate(search_strings):
     match_text, match_conf = fuzzywuzzy.process.extractOne(search_string, detected_strings, scorer=fuzzywuzzy.fuzz.ratio)
-    match_pos = detection['text'].index(match_text)
-    match_left = detection['left'][match_pos]
-    match_top = detection['top'][match_pos]
-    match_width = detection['width'][match_pos]
-    match_height = detection['height'][match_pos]
+    match_pos = detection['text'].index(match_text) # Get match position in detection table
+    print match_pkon['text'][match_pos], detection['conf'][match_pos]
+    row['ocr%d_part%d_text' % (ocr_num, string_num)] = detection['text'][match_pos]
+    row['ocr%d_part%d_ocr_conf' % (ocr_num, string_num)] = detection['conf'][match_pos]
+    row['ocr%d_part%d_match_conf' % (ocr_num, string_num)] = match_conf
+    row['ocr%d_part%d_left' % (ocr_num, string_num)] = detection['left'][match_pos]
+    row['ocr%d_part%d_top' % (ocr_num, string_num)] = detection['top'][match_pos]
+    row['ocr%d_part%d_width' % (ocr_num, string_num)] = detection['width'][match_pos]
+    row['ocr%d_part%d_height' % (ocr_num, string_num)] = detection['height'][match_pos]
 
-df2 = pd.DataFrame({ 'A' : 1.,
-                        'B' : pd.Timestamp('20130102'),
-                        'C' : pd.Series(1,index=list(range(4)),dtype='float32'),
-                        'D' : np.array([3] * 4,dtype='int32'),
-                        'E' : pd.Categorical(["test","train","test","train"]),
-                        'F' : 'foo' })
-
-image name
-patient name
-num_name_parts
-MNR
-found name
-ocr1_name_part1_text
-ocr1_name_part1_conf
-ocr1_name_part1_left
-ocr1_name_part1_top
-ocr1_name_part1_width
-ocr1_name_part1_height
-ocr1_name_partN_*
-orc1_
-
-# fuzzywuzzy.fuzz.ratio
-# fuzzywuzzy.fuzz.partial_ratio
-# fuzzywuzzy.fuzz.token_sort_ratio
-# fuzzywuzzy.fuzz.partial_token_sort_ratio
-# fuzzywuzzy.fuzz.token_set_ratio
-# fuzzywuzzy.fuzz.partial_token_set_ratio
-# fuzzywuzzy.fuzz.WRatio
+ocr_match(row, im_resized, PHI, ocr_num=0)
+ocr_match(row, im_resized, PHI, ocr_num=1)
 
 
-ocr_match(tesseract_config='--oem 0 --psm 3')
 
-text = pytesseract.image_to_string(im_resized,config='--oem 0 --psm 3')
-pytesseract.image_to_string(im_resized,config='--oem 1 --psm 3')
-pytesseract.image_to_string(im_resized,config='--oem 2 --psm 3')
-
-from fuzzywuzzy import fuzz
-
-
-text = pytesseract.image_to_string(data)
-print text
-
-plt.imshow(data, cmap='gray')
-plt.show()
-
-plt.imshow(im_cropped, cmap='gray')
-plt.show()
 
